0f1c1cc0e9ace430cf85fd59efe3df7dcd0ceaa6
Wall-e <you@mail.com>
2023-09-19 23:56:11 +0300

    Задача 8

---
+++ Task04/Program.cs
@@ -0,0 +1,9 @@
+﻿int n, i;
+
+Console.Write("Введите целое число: ");
+while (!int.TryParse(Console.ReadLine(), out n))
+    Console.Write("Данные не корректны, повторите ввод: ");
+for (i = 0; i <= n; i++)
+    if (i % 2 == 0)
+        Console.Write($"{i} ");
+
