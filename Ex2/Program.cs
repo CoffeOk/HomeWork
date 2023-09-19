44b6649404072c4a5a488bc347ff5f1d62d74598
Wall-e <you@mail.com>
2023-09-19 22:10:35 +0300

    Задание 4

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a, b, c, max;
+Console.Write("Введите число a: ");
+while (!int.TryParse(Console.ReadLine(), out a))
+    Console.WriteLine("Введите целое число");
+Console.Write("Введите число b: ");
+while (!int.TryParse(Console.ReadLine(), out b))
+    Console.WriteLine("Введите целое число");
+Console.Write("Введите число c: ");
+while (!int.TryParse(Console.ReadLine(), out c))
+    Console.WriteLine("Введите целое число");
+max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write($"{a}, {b}, {c} -> {max}");
