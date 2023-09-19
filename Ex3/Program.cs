e964de94b92ebd4ca4781fa0bd631dd99ab57cd0
Wall-e <you@mail.com>
2023-09-19 22:26:13 +0300

    Задание 6

---
+++ Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿int x;
+Console.Write("Введите целое число: ");
+while(!int.TryParse(Console.ReadLine(), out x))
+Console.Write("Данные не корректны, повторите ввод: ");
+if (x % 2 == 0)
+{
+    Console.Write($"{x} -> чётное");
+}
+else
+{
+    Console.Write($"{x} -> нечётное");
+}
